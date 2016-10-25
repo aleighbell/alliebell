defed3bfe0a357f4655815f4aa11e1f359b1cb0d
aleighbell <aleighbell@gmail.com>
2016-10-25 15:15:13 -0400

    created blog page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,34 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+   def article_params
+     params.require(:article).permit(:title, :text)
+   end
+
+  def update
+  end
+
+  def destroy
+    @article = Article.destroy(article_params)
+    redirect_to @article
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013173801) do
+ActiveRecord::Schema.define(version: 20161025183701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "authors", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
